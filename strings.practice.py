06a37f4ebad095b4c7e6499acc7c010632b0b491
Ummay Nusra <nusraumme@gmail.com>
2026-09-14 21:18:50 +0600

    added practice-2

---
+++ strings.practice.py
@@ -0,0 +1,10 @@
+# a = '''Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.'''
+# print(a)
+
+
+# checking is a character of string existed
+x = 'hello'
+print('e' in x)
