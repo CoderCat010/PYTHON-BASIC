74bcd1bf97df783ea6fbddde6e6bbb20172b04f7
Ummay Nusra <nusraumme@gmail.com>
2026-09-15 21:54:03 +0600

    added practice-8

--- operators-practice.py
+++ operators-practice.py
@@ -1,18 +1,18 @@
-x = 5
-print(x > 0 and x < 10)
-print(x > 0 or x < 10)
-print(not(x > 0 and x < 10))
+# x = 5
+# print(x > 0 and x < 10)
+# print(x > 0 or x < 10)
+# print(not(x > 0 and x < 10))
 
 
-# identity operatr 
-x = ["apple", "banana"]
-y = ["apple", "banana"]
-x = y
-print(x == y)
-print(x is y)
-print(x is not y)
+# # identity operatr 
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# x = y
+# print(x == y)
+# print(x is y)
+# print(x is not y)
 
 
 # Membership Operators
 fruits = ["apple", "banana", 'orange']
-print('apple' in fruits)
+print('guava' in fruits)
