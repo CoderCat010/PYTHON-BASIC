e9ad4e8dec5543058086c41c9469ae04b7ca1823
Ummay Nusra <nusraumme@gmail.com>
2026-09-15 15:42:40 +0600

    added practice-7

--- operators-practice.py
+++ operators-practice.py
@@ -2,3 +2,12 @@ x = 5
 print(x > 0 and x < 10)
 print(x > 0 or x < 10)
 print(not(x > 0 and x < 10))
+
+
+# identity operatr 
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+x = y
+print(x == y)
+print(x is y)
+print(x is not y)
