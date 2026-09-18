d07c9555876ac9f62b00b0aa7c224fbdd7c0b39c
Ummay Nusra <nusraumme@gmail.com>
2026-09-18 06:40:37 +0600

    added practice-13

--- conditions-practice.py
+++ conditions-practice.py
@@ -8,3 +8,12 @@ if a > b:
     print('a is greater than b')
 elif b > a: 
     print('b is greater than a')
+
+a = 3
+b = 3
+if a > b: 
+    print('a is greater than b')
+elif b > a: 
+    print('b is greater than a')
+elif a == b: 
+    print('no they are not equal')
