bc7bbd6173e40bcbd2a07002d957820677f5f451
Ummay Nusra <nusraumme@gmail.com>
2026-09-18 10:58:56 +0600

    added practice-19

--- conditions-practice.py
+++ conditions-practice.py
@@ -38,3 +38,32 @@ a = 2
 b = 5
 if not a > 5: 
     print('a is not greater than b')
+
+
+age = 25
+is_student = False
+has_discount_code = True
+if (age > 18 or age < 60) and not is_student or has_discount_code: 
+    print('Discount applies!')
+
+
+'''
+Problem 1:
+input:
+একটা ব্যক্তির বয়স (age) input হিসেবে দেওয়া থাকবে। তুমি প্রোগ্রাম লিখবে যেটা বলবে সে কোন category-তে পড়ে:
+
+output: 
+- বয়স 0-12 হলে → "Child"
+- বয়স 13-19 হলে → "Teenager"
+- বয়স 20-59 হলে → "Adult"
+- বয়স 60 বা তার বেশি হলে → "Senior"
+'''
+enterYourAge = int(input())
+if (enterYourAge >= 0 and enterYourAge <= 12):
+    print(enterYourAge, 'child')
+elif (enterYourAge >= 13 and enterYourAge <= 19):
+    print(enterYourAge, 'teenager')
+elif (enterYourAge >= 20 and enterYourAge <= 59):
+    print(enterYourAge, 'adult')
+else: 
+    print(enterYourAge, 'old')
