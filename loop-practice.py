03a7edf1cae2b2bbf5a2aa5c18e84a9433b3e654
Ummay Nusra <nusraumme@gmail.com>
2026-09-21 13:15:14 +0600

    added practice-20

---
+++ loop-practice.py
@@ -0,0 +1,19 @@
+# count = 0
+# while count <= 3:
+#     print(count)
+#     count += 1
+
+count = 0
+while count <= 3:
+    print(count)
+    if count == 3:
+        break
+    count += 1
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
+
