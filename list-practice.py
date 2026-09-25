63dad7b7b8f06faee5118c5eec97565cf08e30d1
Ummay Nusra <nusraumme@gmail.com>
2026-09-25 19:10:39 +0600

    added practice-29

--- list-practice.py
+++ list-practice.py
@@ -1,22 +1,28 @@
 # x = ['x', 'y', 6, 1.5, -8]
 # print(x)
 
-# methods
-x = [1, 2, 3, 4]
-x.append(5)
-print(x)
+# # methods
+# x = [1, 2, 3, 4]
+# x.append(5)
+# print(x)
 
-# insert ---- to add items
-a = [1, 2, 3, 5]
-a.insert(3,'4')
-a.insert(10,'4')
-a.insert(-10,'4')
-print(a)
+# # insert ---- to add items
+# a = [1, 2, 3, 5]
+# a.insert(3,'4')
+# a.insert(10,'4')
+# a.insert(-10,'4')
+# print(a)
 
-# pop method --- to remove item from the last
-fruits = ["apple", "banana", "mango"]
-removeElm = fruits.pop()
-print(removeElm)
-print(fruits)
-print(fruits.pop(1))
-print(fruits)
+# # pop method --- to remove item from the last
+# fruits = ["apple", "banana", "mango"]
+# removeElm = fruits.pop()
+# print(removeElm)
+# print(fruits)
+# print(fruits.pop(1))
+# print(fruits)
+
+# remove method --- remove item's by their names instead of index
+x = ['a', 'hh', 'jj']
+if 'a'in x:
+    x.remove('a')
+    print(x)
